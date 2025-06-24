8f288635f2146612e6cd8df1bde2fb999d9e76d3
Dominic Brian <dominic91960@gmail.com>
2025-06-24 14:20:38 +0530

    feat: add new sign in component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import SignIn from './Pages/Sign-in';
-import SignUp from './Pages/Sign-up';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/Sign-up";
 
 function App() {
-
-	return (
-		<div>
-			<BrowserRouter>
-				<Routes>
-
- 					<Route path='/sign-in' element={<SignIn />} />
- 					<Route path='/sign-up' element={<SignUp />} />
-
-				</Routes>
-			</BrowserRouter>
-
-		</div>
-
-	)
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
